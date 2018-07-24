3adc54010cc46b6a1539648f9d8335c11fbd1ab7
Alessandro <a.bellotti@patente.it>
2018-07-24 11:47:50 +0200

    Updated for Requires Changes

--- XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
@@ -31,9 +31,6 @@ import com.example.xyzreader.R;
 
 public class DrawInsetsFrameLayout extends FrameLayout {
     private Drawable mInsetBackground;
-    private Drawable mTopInsetBackground;
-    private Drawable mBottomInsetBackground;
-    private Drawable mSideInsetBackground;
 
     private Rect mInsets;
     private Rect mTempRect = new Rect();
